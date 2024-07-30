b2aae3c152db9199e8841603f0e5a413fec75737
Mariya <mariaomware@gmail.com>
2024-07-30 12:02:18 +0300

    Initialized db

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'mysecret')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://myuser:mypassword@localhost/mydatabase')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:newpassword@localhost/mydatabase')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
