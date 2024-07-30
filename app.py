16dc625464278d83f2dae10b5c0a0356a053dd77
Migero <mikeroche138@gmail.com>
2024-07-30 15:03:26 +0300

    add model views

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from models import db, User
+from models import db, User, Role, Income, IncomeCategory, Expense, ExpenseCategory, Debt, DebtPayment, FinancialReport, Transaction, Asset, SavingsGoal, Setting
 import config
 
 app = Flask(__name__)
@@ -15,6 +15,18 @@ admin = Admin(app, name='MyApp', template_mode='bootstrap3')
 
 # Add model views to Flask-Admin
 admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Role, db.session))
+admin.add_view(ModelView(Income, db.session))
+admin.add_view(ModelView(IncomeCategory, db.session))
+admin.add_view(ModelView(Expense, db.session))
+admin.add_view(ModelView(ExpenseCategory, db.session))
+admin.add_view(ModelView(Debt, db.session))
+admin.add_view(ModelView(DebtPayment, db.session))
+admin.add_view(ModelView(FinancialReport, db.session))
+admin.add_view(ModelView(Transaction, db.session))
+admin.add_view(ModelView(Asset, db.session))
+admin.add_view(ModelView(SavingsGoal, db.session))
+admin.add_view(ModelView(Setting, db.session))
 
 @app.route('/')
 def index():
